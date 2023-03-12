4b6bdd486a9795d90bc6d5aaa5929434bc92bcd4
Ivo Slegers <ivo.m.slegers@gmail.com>
2023-03-12 14:08:34 +0100

    Implemented optimal value function iteration algorithm.

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -56,25 +56,30 @@ class Policy:
         return np.array_equal(self.action_probabilities, other.action_probabilities)
 
 
+def calculate_state_transition_matrix(mdp: MarkovDecisionProcess, policy: Policy) -> np.ndarray:
+    return np.einsum("ij,jik->ik", policy.action_probabilities, mdp.transition_probabilities)
+
+
+# value_function.shape = (#states), value_function[i] is the value function associated to state i
 def calculate_value_function(mdp: MarkovDecisionProcess, policy: Policy) -> np.ndarray:
     assert mdp.n_states == policy.n_states
     assert mdp.n_actions == policy.n_action
 
-    T = np.einsum("ij,jik->ik", policy.action_probabilities, mdp.transition_probabilities)
+    T = calculate_state_transition_matrix(mdp, policy)
     return np.linalg.pinv(np.identity(mdp.n_states) - mdp.discount * T) @ T @ mdp.rewards
 
 
-def calculate_value_function_iterative(mdp: MarkovDecisionProcess, policy: Policy, max_steps: int=100) -> np.ndarray:
+def calculate_value_function_iterative(mdp: MarkovDecisionProcess, policy: Policy, max_iterations: int=100) -> np.ndarray:
     assert mdp.n_states == policy.n_states
     assert mdp.n_actions == policy.n_action
 
-    T = np.einsum("ij,jik->ik", policy.action_probabilities, mdp.transition_probabilities)
+    T = calculate_state_transition_matrix(mdp, policy)
     v = np.zeros(shape=mdp.n_states)
 
-    for _ in range(max_steps):
+    for _ in range(max_iterations):
         v_new = T @ (mdp.rewards + mdp.discount * v)
 
-        if (v == v_new).all():
+        if np.array_equal(v, v_new):
             break
 
         v = v_new
@@ -83,12 +88,47 @@ def calculate_value_function_iterative(mdp: MarkovDecisionProcess, policy: Polic
 
 
 # action_value_function.shape = (#states, #action) with action_value_function[i, j] is the action value (q) for taking action j when in state i
-def calculate_action_value_function(mdp: MarkovDecisionProcess, policy: Policy) -> np.ndarray:
-    value_function = calculate_value_function(mdp, policy)
+def calculate_action_value_function(mdp: MarkovDecisionProcess, value_function: np.ndarray) -> np.ndarray:
+    assert value_function.ndim == 1 and value_function.shape[0] == mdp.n_states
+
     action_value_function_transposed = mdp.transition_probabilities @ (mdp.rewards + mdp.discount * value_function)
     return action_value_function_transposed.T
+
+
+def calculate_action_value_function_from_policy(mdp: MarkovDecisionProcess, policy: Policy) -> np.ndarray:
+    value_function = calculate_value_function(mdp, policy)
+    return calculate_action_value_function(mdp, value_function)
 
 
 def calculate_greedy_policy(action_value_function) -> np.ndarray:
     is_max_action = action_value_function == action_value_function.max(axis=-1, keepdims=True)
     return Policy(is_max_action / is_max_action.sum(axis=-1, keepdims=True))
+
+
+def policy_iteration(mdp: MarkovDecisionProcess, initial_policy: Policy, max_iterations: int) -> Policy:
+    policy = initial_policy
+
+    for _ in range(max_iterations):
+        action_value_function = calculate_action_value_function_from_policy(mdp, policy)
+        new_policy = calculate_greedy_policy(action_value_function)
+
+        if policy == new_policy:
+            break
+        policy = new_policy
+    
+    return policy
+
+
+def calculate_optimal_value_function(mdp: MarkovDecisionProcess, max_iterations: int) -> np.ndarray:
+
+    v = np.zeros(shape=mdp.n_states)
+
+    for _ in range(max_iterations):
+        new_v = (mdp.transition_probabilities @ (mdp.rewards + mdp.discount * v)).max(axis=0)
+
+        if np.array_equal(v, new_v):
+            break
+
+        v = new_v
+
+    return v
